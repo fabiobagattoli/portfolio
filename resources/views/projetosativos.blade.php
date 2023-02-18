73c1ef5667d253c2f9c59b1d4feb399e19449033
fabiobagattoli <122814160+fabiobagattoli@users.noreply.github.com>
2023-02-18 11:54:28 -0200

    alteração de conteúdo

--- resources/views/projetosativos.blade.php
+++ resources/views/projetosativos.blade.php
@@ -55,7 +55,7 @@
             <div class="col-sm-6 col-md-4">
                 <div class="article-summary">
                     <div class="article-img"><img src="assets/images/centerbusca.png" alt="" /></div>
-                    <div class="article-title"><a href="https://www.centerbusca.com/" target="_blank">Center Busca: www.centerbusca.com</a></div>
+                    <div class="article-title"><a href="https://www.meiweb.com.br/classificados/" target="_blank">Classificados</a></div>
                     <div class="article-text">Site de classificados integrado com o sistema MEI WEB. 
                     </div>
                     <div class="article-links row">
@@ -66,7 +66,7 @@
                         </div>
                         <div class="col-xs-6">
                             <div class="readmore-holder">
-                                <a href="https://www.centerbusca.com/" target="_blank" class="ybtn ybtn-small ybtn-accent-color-text">Acessar site</a>
+                                <a href="https://www.classificados/classificados/" target="_blank" class="ybtn ybtn-small ybtn-accent-color-text">Acessar site</a>
                             </div>
                         </div>
                     </div>
